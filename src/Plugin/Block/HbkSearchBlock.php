cdcf3519ba18c94a3ff4b4d4411a3ea8f2874459
kouwa stephane <kksasteph888@gmail.com>
2024-01-12 19:16:30 +0100

    update search block

--- src/Plugin/Block/HbkSearchBlock.php
+++ src/Plugin/Block/HbkSearchBlock.php
@@ -17,64 +17,85 @@ use Drupal\search\Plugin\Block\SearchBlock;
  * Provides a 'Search form' block.
  *
  * @Block(
- *   id = "hbk_search_form_block",
+ *   id = "formatage_models_search_form_block",
  *   admin_label = @Translation("Hbk Search form"),
  *   category = @Translation("Forms")
  * )
  */
 class HbkSearchBlock extends SearchBlock {
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration() {
-        return [
-            'page_id' => '',
-            'template' => 'default_template'
-        ];
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'template_render' => 'formatage_models_search_with_icon',
+      'class_input' => 'h3'
+    ] + parent::defaultConfiguration();
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function build() {
+    $form = parent::build();
+    $template = $this->configuration['template_render'];
+    $config = $this->getConfiguration();
+    if ($template) {
+      if (!empty($form['actions']['submit'])) {
+        $form['actions']['submit']['#theme_wrappers'][] = 'input_submit_blm';
+      }
+      if (!empty($form['keys'])) {
+        $form['keys']['#attributes']['placeholder'] = $form['keys']['#attributes']['title'];
+        if (!empty($form['keys']['#attributes']['class'])) {
+          $form['keys']['#attributes']['class'][] = 'blm-key-search';
+          $form['keys']['#attributes']['class'][] = $config['class_input'];
+        }
+        else {
+          $form['keys']['#attributes']['class'] = [
+            'blm-key-search',
+            $config['class_input']
+          ];
+        }
+      }
+      return [
+        "#theme" => $template,
+        "#content" => $form
+      ];
     }
-
-
-
-    /**
-     * {@inheritdoc}
-     */
-    public function build() {
-        $page = $this->configuration['page_id'] ?? NULL;
-        $form = $this->formBuilder->getForm(SearchBlockForm::class, $page);
-        // $form["#theme"][0] = "hbk_search_block_form";
-        dump($form);
-        return [
-            "#theme" => "my_custom_theme",
-            "content" => $form,
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function blockSubmit($form, FormStateInterface $form_state) {
-        $this->configuration['page_id'] = $form_state->getValue('page_id');
-        $this->configuration['template'] = $form_state->getValue('template');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function blockForm($form, FormStateInterface $form_state) {
-        $form = parent::blockForm($form, $form_state);
-        $form["page_id"]["#default_value"] = $this->configuration["page_id"];
-
-        $options = [
-            "default_template" => $this->t("Default Template"),
-            "submit_in_field" => $this->t("Submit in field")
-        ];
-        $form["template"] = [
-            "#type" => "select",
-            "#title" => $this->t("Search Block Template"),
-            "#description" => $this->t("Template that will render the search form"),
-            "#default_value" => $this->configuration["template"] ?? $options["default_template"],
-            "#options" => $options
-        ];
-        return $form;
-    }
+    else
+      return $form;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $this->configuration['template_render'] = $form_state->getValue('template_render');
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    $options = [
+      "" => $this->t("Default Template"),
+      "formatage_models_search_with_icon" => $this->t("Search form with icon")
+    ];
+    $form["template_render"] = [
+      "#type" => "select",
+      "#title" => $this->t("Search Block Template"),
+      "#description" => $this->t("Template that will render the search form"),
+      "#default_value" => $this->configuration["template_render"],
+      "#options" => $options
+    ];
+    return $form;
+  }
+  
 }
